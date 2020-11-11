3df0dd4bd4d6fd0797400bd36eaea40d44696ce5
Samuel-Fox <foxsam@oregonstate.edu>
2020-11-11 14:19:19 -0800

    Demo area
    The player character has been made with a camera that follows them and a movement system. A few booths have been placed to show the movement.

---
+++ CS 461 MMO Expo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent < NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
