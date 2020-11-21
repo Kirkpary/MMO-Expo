2237eac1a7194537f9ee9e8180705142927bb4ac
zexianliOSU <lizexi@oregonstate.edu>
2020-11-20 20:24:46 -0800

    Added script to cycle through characters

---
+++ Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelection : MonoBehaviour
+{
+    // All the characters options goes into this list
+    public GameObject[] characters;
+    public int selectedCharacter = 0;
+
+    public void NextCharacter()
+    {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter = (selectedCharacter + 1) % characters.Length;
+        characters[selectedCharacter].SetActive(true);
+    }
+
+    public void PreviousCharacter()
+    {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter--;
+        if (selectedCharacter < 0)
+        {
+            selectedCharacter += characters.Length;
+        }
+        characters[selectedCharacter].SetActive(true);
+    }
+}
