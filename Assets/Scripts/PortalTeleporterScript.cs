f78e1d42f35ebf0ea5c0783519212f517cb1da87
Min-Chew <chewminwei@yahoo.com>
2020-11-30 16:17:07 -0800

    Added portal

---
+++ Assets/Scripts/PortalTeleporterScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.Oregonstate.MMOExpo
+{
+    public class PortalTeleporterScript : MonoBehaviour
+    {
+
+        void OnTriggerEnter (Collider other) 
+        {
+            if (SceneManager.GetActiveScene().name == "CS_Room")
+            {
+                Application.LoadLevel("ECE_Room");
+            }
+            else
+            {
+                Application.LoadLevel("CS_Room");
+            }
+        }
+    }
+}   
