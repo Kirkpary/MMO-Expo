9aa4f637068d344bbcd5ee9331282e2c19a8af23
Ryan Kirkpatrick <kirkpary@oregonstate.edu>
2021-01-29 15:33:54 -0800

    Testing JSON fix

--- prototype/Build/Build.framework.js
+++ prototype/Build/Build.framework.js
@@ -1307,7 +1307,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2973200;
+STATICTOP = STATIC_BASE + 2974640;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3237,7 +3237,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2973200;
+var STATIC_BUMP = 2974640;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -15844,8 +15844,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 81666;
-Module["wasmMaxTableSize"] = 81666;
+Module["wasmTableSize"] = 81668;
+Module["wasmMaxTableSize"] = 81668;
 function invoke_dd(index, a1) {
  var sp = stackSave();
  try {
