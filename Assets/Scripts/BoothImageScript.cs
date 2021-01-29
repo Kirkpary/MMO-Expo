45352b4018d63f21aa50192acb54df67c6623833
Samuel-Fox <foxsam@oregonstate.edu>
2021-01-28 19:19:05 -0800

    BoothUI progress

---
+++ Assets/Scripts/BoothImageScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoothImageScript : MonoBehaviour
+{
+
+    public GameObject BoothUI;
+    public GameObject BoothHover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoothUI.SetActive(false);
+        BoothHover.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+       if(Input.GetMouseButtonDown(0) & BoothUI.active == false)
+        {
+            BoothUI.SetActive(true);
+        }
+
+       else
+        {
+            BoothUI.SetActive(false);
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (BoothHover.active == false)
+        {
+            BoothHover.SetActive(true);
+            Debug.Log("Mouse is over GameObject.");
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (BoothHover.active == true)
+        {
+            BoothHover.SetActive(false);
+        }
+    } 
+}
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject playerCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            playerCamera.transform.Rotate(0, 0, 0.2f, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerCamera.transform.Rotate(0, 0, -0.2f, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            playerCamera.transform.Rotate(.2f, 0, 0, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            playerCamera.transform.Rotate(-.2f, 0, 0, Space.Self);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            playerCamera.transform.Translate(0, 0, .2f, Space.Self);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            playerCamera.transform.Translate(0, 0, -.2f, Space.Self);
+        }
+    }
+}
