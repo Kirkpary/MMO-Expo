e6d284f8e34b17bf05ce7aec322023b761b772e8
zexianliOSU <lizexi@oregonstate.edu>
2021-02-01 23:19:22 -0800

    Open and Close search UI

---
+++ Assets/Scripts/SearchScript.cs
@@ -0,0 +1,33 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchScript : MonoBehaviour
+{
+    public GameObject popUpContainer;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        popUpContainer.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenSearchContainer()
+    {
+        Debug.Log("Button");
+        popUpContainer.SetActive(true);
+    }
+
+    public void CloseSearchContainer()
+    {
+        popUpContainer.SetActive(false);
+    }
+}
