e622f76f553fb125599c9f0e20c5e83f069e2b7f
Ryan Kirkpatrick <kirkpary@oregonstate.edu>
2021-02-03 13:55:01 -0800

    Added spawn point prefab for player and booth
    + Added spawn point prefab for player and booth with wire mesh display
    + Added toggle for camera rotation to camera follow script
    + Created a prefab for booth instantiation
    - Deleted demo scene for Low Poly Starter Pack

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.Oregonstate.MMOExpo
+{
+    public class SpawnPoint : MonoBehaviour
+    {
+        public MeshFilter PrefabMesh;
+        public Transform PrefabTransform;
+
+        private void OnDrawGizmos()
+        {
+            Vector3 position = transform.position;
+            if (PrefabMesh != null && PrefabTransform != null)
+            {
+                position.y += PrefabMesh.sharedMesh.bounds.extents.y;
+                Gizmos.DrawWireMesh(PrefabMesh.sharedMesh, -1, position, Quaternion.identity, PrefabTransform.localScale);
+            }
+        }
+    }
+}
