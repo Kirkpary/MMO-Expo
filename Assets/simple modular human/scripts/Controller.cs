5610fe1f6b05bae339cb5101e8b3b5b8281458fa
zexianliOSU <lizexi@oregonstate.edu>
2021-03-05 12:10:10 -0800

    Movement to new chacraters

---
+++ Assets/simple modular human/scripts/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Controller : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Animator animator;
+    public bool execute_walking;
+    public bool walk;
+
+    public float walk_speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
